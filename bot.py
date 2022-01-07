8a1ecc447f689701a682dc42fadd4d5eb27841fc
kranolabar <kranola@outlook.com>
2022-01-07 16:33:55 +0800

    added telegram bot integration

--- bot.py
+++ bot.py
@@ -1,28 +1,58 @@
 import urllib.request
-
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler, MessageHandler, Filters
+import requests
+import os
 import re
 
-# This uses bs4 to extract all the links in the english page
-song_choice = "just"
+PORT = int(os.environ.get('PORT', 5000))
+TOKEN = os.environ["5052808423:AAGL9xY4r16WfPsAr111BJiQcUuOoymJ1lQ"]
 
-req = Request("https://glccsongbook.github.io/english.html")
-html_page = urlopen(req)
+def song(update, context):
+    text = update.message.text
+    def get_song_url():
+        song_choice = text
+        req = Request("https://glccsongbook.github.io/english.html")
+        html_page = urlopen(req)
 
-soup = BeautifulSoup(html_page, "lxml")
+        soup = BeautifulSoup(html_page, "lxml")
 
-links = []
-for link in soup.findAll('a'):
-    links.append(link.get('href'))
+        links = []
+        for link in soup.findAll('a'):
+            links.append(link.get('href'))
 
-song_choice = song_choice.replace(" ", "-").replace(",", "")
-pattern = re.compile(song_choice)
-str_match = [x for x in links if re.search(song_choice, x)]
+        song_choice = song_choice.replace(" ", "-").replace(",", "")
+        pattern = re.compile(song_choice)
+        str_match = [x for x in links if re.search(song_choice, x)]
 
-song_url = "https://glccsongbook.github.io"+''.join(map(str, str_match)).replace('.', '', 1)
+        song_url = "https://glccsongbook.github.io" + ''.join(map(str, str_match)).replace('.', '', 1)
 
-soup = BeautifulSoup(urllib.request.urlopen(song_url),"lxml")
-result = soup.find_all("div", {"class":"bs-component"})
+        return song_url
 
-print(result[1].text)
+    def print_lyrics():
+        song_url = get_song_url()
+        soup = BeautifulSoup(urllib.request.urlopen(song_url), "lxml")
+        result = soup.find_all("div", {"class": "bs-component"})
+        lyrics = result[1].text
+
+        return lyrics
+    lyrics = print_lyrics()
+    chat_id = update.message.chat.id
+    context.bot.send_message(chat_id=chat_id, text=lyrics)
+    return
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('song', song))
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://your-app-name.herokuapp.com/' + TOKEN)
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
