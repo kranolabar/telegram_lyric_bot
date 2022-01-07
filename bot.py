3d36e38571c547b43ec94d4c13cff408e7f9a638
kranolabar <kranola@outlook.com>
2022-01-07 14:51:57 +0800

    basic logic for lyric bot created

--- bot.py
+++ bot.py
@@ -1,21 +1,28 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHelper
-import requests
+import urllib.request
+
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
 import re
-import os
 
-PORT = int(os.environ.get('PORT', 5000))
-TOKEN = os.environ["TOKEN"]
-API_ENDPOINT = ""
+# This uses bs4 to extract all the links in the english page
+song_choice = "just"
 
-# function to obtain the URL of
-def get_url():
-    contents = requests.get(API_ENDPOINT).json()
-    url = contents['message']
-    return url
+req = Request("https://glccsongbook.github.io/english.html")
+html_page = urlopen(req)
 
-def get_lyric_url
+soup = BeautifulSoup(html_page, "lxml")
 
-def lyric(update, context):
-    url = get_lyric_url()
-    chat_id = update.message.chat.id
-    context.bot.send_message(chat_id=chat_id,text=url)
+links = []
+for link in soup.findAll('a'):
+    links.append(link.get('href'))
+
+song_choice = song_choice.replace(" ", "-").replace(",", "")
+pattern = re.compile(song_choice)
+str_match = [x for x in links if re.search(song_choice, x)]
+
+song_url = "https://glccsongbook.github.io"+''.join(map(str, str_match)).replace('.', '', 1)
+
+soup = BeautifulSoup(urllib.request.urlopen(song_url),"lxml")
+result = soup.find_all("div", {"class":"bs-component"})
+
+print(result[1].text)
